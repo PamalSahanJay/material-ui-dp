20f60057f9fad0c654d21623ffb4def2760c75bd
psahan <psahan@wiley.com>
2024-07-02 07:48:22 +0530

    add all

---
+++ apps/mui-demo/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="mui-demo" />
+    </div>
+  );
+}
+
+export default App;
